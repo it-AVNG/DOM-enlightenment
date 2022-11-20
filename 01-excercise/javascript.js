3bae14d4174aceda8c4a36bec0110c8aef9619fa
it-AVNG <it.avng@gmail.com>
2022-11-20 10:11:09 +0700

    [clean up] Javascript for Excercise 01

--- 01-excercise/javascript.js
+++ 01-excercise/javascript.js
@@ -20,21 +20,23 @@ header3.style.color = 'blue';
 
 container.appendChild(header3);
 
-// a <div> with a black border and pink background color 
-//with the following elements inside of it:
-//     another <h1> that says “I’m in a div”
-//     a <p> that says “ME TOO!”
-//     Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+ 
 
+// a <div> with a black border and pink background color
 const block = document.createElement('div');
 block.classList.add('block');
 block.style.cssText ='border-color: black;border-style: solid;background-color:pink;border-width:2px;';
 container.appendChild(block);
 
+
+//with the following elements inside of it:
+//     another <h1> that says “I’m in a div”
 const header1 = document.createElement('h1');
 header1.textContent = "I'm in a div";
 block.appendChild(header1);
 
+
+//     a <p> that says “ME TOO!”
 const p2 = document.createElement('p2');
 p2.textContent = "ME TOO!";
 block.appendChild(p2);
